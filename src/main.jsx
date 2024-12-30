86e9487be595b70e8b22a01a251627d2834edc0a
kevin <kevin.o.nicolas.m@gmail.com>
2024-12-29 22:38:38 -0300

    [FIX] Error relacionado al context eliminado

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {PortContextProvider} from './context/PortContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App/>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
